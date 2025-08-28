3ad55c9cab4c31835757014140d68327568e8dd1
Jawwad Ahsan <jawwad654321@gmail.com>
2025-08-28 22:07:05 +0500

    weather object handling going on

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let [weather, setWeather] = useState(0)
+  useEffect(()=>{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=aa9978b91d5b6560db0ffa88c6696e01&units=metric').then((data)=>data.json()).then((data)=>{
+    console.log(data)
+    // data.main.temp
+    // data.main.temp_max
+    // data.main.temp_min
+    // data.main.feels_like
+    // data.main.pressure
+    // data.main.temp
+    // data.main.humidity
+    // data.name
+    // data.sys.sunrise
+    // data.sys.sunset
+    // data.timezone
+    // data.visibility
+    // data.weather[0].main
+    //  data.weather[0].description
+    // data.wind.speed
+    // data.wind.gust
+    // data.wind.deg
+    setWeather(data)
+  }).catch((err)=>{
+    console.log(err);
+  })
+  },[])
   return (
     <div>
       
